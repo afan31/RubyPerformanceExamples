9829c4e4cd4756f8e0070b2a56c8a974787fb495
Afan <afan.khan50@gmail.com>
2018-09-12 07:09:39 -0400

    wrapper example, yet to be finished

--- ch2/wrapper_example.rb
+++ ch2/wrapper_example.rb
@@ -1,7 +1,12 @@
-require 'wrapper'
+require './wrapper'
 require 'csv'
 
 measure do
+  test_data = Array.new(100000){Array.new(10){"x"*1000}}
+  File.open("data.csv", "w+") {|f| f.write_test_data.join("\n")}
   data = CSV.open("data.csv")
-  puts data
+  output = data.readlines.map do |line|
+    line.map {|col| col.downcase.gsub(/\b('?[a-z])/){$1.capitalize}}
+  end
+  File.open("output.csv", "w+") {|f| f.write_output.join("\n")}
 end

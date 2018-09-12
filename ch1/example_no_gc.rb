1cad8dbbd67343222360cd894a86f8b575067bd1
Afan <afan.khan50@gmail.com>
2018-09-11 20:36:46 -0400

    Example with garbage collection disabled

---
+++ ch1/example_no_gc.rb
@@ -0,0 +1,10 @@
+require "benchmark"
+
+nr = 100000
+nc = 10
+data = Array.new(nr){Array.new(nc){"x"*1000}}
+GC.disable
+time = Benchmark.realtime do
+  csv = data.map{ |row| row.join(",")}.join("\n")
+end
+puts time.round(2)
